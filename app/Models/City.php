d89e417924200af6eb503025e7f9c03742fe8a0b
hoesnial <husni2518@gmail.com>
2024-12-26 07:51:11 +0700

    first commit

---
+++ app/Models/City.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class City extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    protected $fillable = [
+        'name',
+        'slug', // domain.com/Bandung
+        'photo',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = ucwords($value);
+        $this->attributes['slug'] = tr::slug($value);
+    }
+
+
+    public function gyms(): HasMany
+    {
+        return $this->hasMany(Gym::class);
+    }
+}
